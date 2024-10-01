7d8501f63bf36e951ed488722ed8bf8e77d3b071
luthomas <Thomas.Lu@garmin.com>
2024-10-01 10:34:26 +0800

    31. Imporved Maxsum

--- 4. Introduction to Algorithm Design/SlidingWindow.js
+++ 4. Introduction to Algorithm Design/SlidingWindow.js
@@ -1,34 +1,25 @@
-maxSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 2); // 12
-minSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 3); // -28
+maxSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 3); // 12
+// minSum([2, 7, 3, 0, 6, 1, -5, -12, -11], 3); // -28
 
 function maxSum(arr, size) {
-  let max_value = -Infinity;
-
   if (size > arr.length) {
-    return null; // 我們找不到這樣的 Sliding Window
+    return null;
   }
 
-  for (let i = 0; i <= arr.length - size; i++) {
-    let window = [];
-    let attempt = 0;
-    for (let j = i; j < i + size; j++) {
-      // console.log(i, j);
-      window.push(arr[j]);
-      attempt += arr[j];
-    }
-    /**
-     * [ 2, 7, 3 ]
-       [ 7, 3, 0 ]
-       [ 3, 0, 6 ]
-       [ 0, 6, 1 ]
-       [ 6, 1, -5 ]
-       [ 1, -5, -12 ]
-     */
-    // console.log(window);
-    if (attempt > max_value) {
-      max_value = attempt;
+  let max_value = 0;
+  for (let i = 0; i < size; i++) {
+    max_value += arr[i];
+  }
+
+  // 再做第二個循環，起點由 0 開始
+  let tempValue = max_value;
+  for (let j = size; j < arr.length; j++) {
+    tempValue = tempValue + arr[j] - arr[j - size];
+    if (tempValue > max_value) {
+      max_value = tempValue;
     }
   }
+
   console.log(max_value);
   return max_value;
 }
