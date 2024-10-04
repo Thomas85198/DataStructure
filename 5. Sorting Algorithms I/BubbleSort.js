d22c45e0c7604a9b6004ed0c7718bfa5eec463c5
luthomas <Thomas.Lu@garmin.com>
2024-10-04 21:26:12 +0800

    41. Bubble Sort

---
+++ 5. Sorting Algorithms I/BubbleSort.js
@@ -0,0 +1,27 @@
+function bubbleSort(arr) {
+  let step = 0;
+  for (let i = 0; i <= arr.length - 2; i++) {
+    for (let j = arr.length - 1; j >= i + 1; j--) {
+      if (arr[j] < arr[j - 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+        step++;
+      }
+    }
+  }
+  console.log("It takes " + step + " steps to complete");
+  console.log(arr);
+}
+
+bubbleSort([4, 1, 5, 2, 7]);
+
+let test = [];
+
+for (let i = 0; i < 100; i++) {
+  test.push(Math.floor(Math.random() * 100));
+}
+
+console.log(test);
+bubbleSort(test);
+// Time complexity: O(n^2)
