0c64e481ea91846a2cbb86ea52744bfb57b3222e
luthomas <Thomas.Lu@garmin.com>
2024-10-05 16:59:43 +0800

    42. Big O of Bubble Sort

--- 5. Sorting Algorithms I/BubbleSort.js
+++ 5. Sorting Algorithms I/BubbleSort.js
@@ -1,20 +1,27 @@
 function bubbleSort(arr) {
   let step = 0;
   for (let i = 0; i <= arr.length - 2; i++) {
+    let swapping = false;
     for (let j = arr.length - 1; j >= i + 1; j--) {
       if (arr[j] < arr[j - 1]) {
         let temp = arr[j];
         arr[j] = arr[j - 1];
         arr[j - 1] = temp;
         step++;
+        swapping = true;
+        console.log(arr);
       }
     }
+    if (swapping === false) {
+      break;
+    }
   }
+
   console.log("It takes " + step + " steps to complete");
   console.log(arr);
 }
 
-bubbleSort([4, 1, 5, 2, 7]);
+// bubbleSort([4, 1, 5, 2, 7]);
 
 let test = [];
 
@@ -22,6 +29,8 @@ for (let i = 0; i < 100; i++) {
   test.push(Math.floor(Math.random() * 100));
 }
 
-console.log(test);
-bubbleSort(test);
+// console.log(test);
+// bubbleSort(test);
 // Time complexity: O(n^2)
+
+bubbleSort([1, 2, 3, 4, 0, 5, 6, 7]);
